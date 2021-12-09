84aa2105e30c0009701fec93beec11bada1839d2
Thomas WEst <thom8750@stud.kea.dk>
2021-12-09 12:41:31 +0100

    data structure

--- app.js
+++ app.js
@@ -2,3 +2,31 @@ import express from 'express';
 
 const app = express();
 app.use(express.static("public"));
+
+const frontpagePage = createPage("frontpage/index.html", {
+    title: "Phone-Rep | Welcome"
+});
+
+const repairPage = createPage("frontpage/repair.html", {
+    title: "Phone-Rep| Repairs"
+});
+
+const frontpagePage = createPage("frontpage/shops.html", {
+    title: "Nodefolio | Shops"
+});
+
+
+app.get('/', function (req, res) {
+    res.send(frontpagePage)
+  });
+
+app.get('/repairs', function (req, res) {
+    res.send(repairPage)
+  });
+
+
+  app.get('/shops', function (req, res) {
+    res.send(shopPage)
+  });
+
+
